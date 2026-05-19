a5ab34ba0f77d115a8af2ecd6bf9ac2d98de16ac
Denis Ballgjini <deni.ballgjini01@gmail.com>
2026-05-19 20:00:40 +0200

    Built AI Study Assistant MVP

---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request
+from PyPDF2 import PdfReader
+from collections import Counter
+import os
+import re
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+def generate_questions(text):
+
+    words = re.findall(r'\b[a-zA-Z]{6,}\b', text)
+
+    common_words = Counter(words)
+
+    topics = []
+
+    for word, count in common_words.most_common(3):
+        topics.append(word)
+
+    questions = []
+
+    for topic in topics:
+
+        questions.append(
+            f"What is {topic}?"
+        )
+
+        questions.append(
+            f"Explain the role of {topic}."
+        )
+
+    return questions
+
+
+def extract_concepts(text):
+
+    words = re.findall(
+        r'\b[a-zA-Z]{6,}\b',
+        text
+    )
+
+    common_words = Counter(words)
+
+    concepts = []
+
+    for word, count in common_words.most_common(5):
+        concepts.append(word)
+
+    return concepts
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    file = request.files["pdf"]
+
+    if file:
+
+        filepath = os.path.join(
+            app.config["UPLOAD_FOLDER"],
+            file.filename
+        )
+
+        file.save(filepath)
+
+        reader = PdfReader(filepath)
+
+        text = ""
+
+        for page in reader.pages:
+
+            extracted = page.extract_text()
+
+            if extracted:
+                text += extracted
+
+        summary = text[:700]
+
+        quiz = generate_questions(text)
+
+        concepts = extract_concepts(text)
+
+        return render_template(
+            "summary.html",
+            summary=summary,
+            quiz=quiz,
+            concepts=concepts
+        )
+
+    return "No file uploaded"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
